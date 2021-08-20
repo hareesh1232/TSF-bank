195986209c348228cab6ed6336085a8da9df2933
hareesh1232 <55019318+hareesh1232@users.noreply.github.com>
2021-08-20 07:57:34 +0530

    https://hkr078.000webhostapp.com/
    https://hkr078.000webhostapp.com/

---
+++ README.md
@@ -0,0 +1,5 @@
+# GRIP-BANKING-SYSTEM
+
+### Website Link - https://pkr2ndhere.000webhostapp.com/
+
+###### GRIPJUNE2021 || WEB DEVELOPMENT & DESIGNING INTERN || THE SPARKS FOUNDATION
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+	$conn = mysqli_connect('localhost','id17055105_pkrgripp','Pathikdas!33','id17055105_pkrgrip');
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+	?>
